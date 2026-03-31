c2d8f0d6a5742f33fb8f2c8086c6f11a8c804f8e
Rishabh Prakash <rishabh.prakash@example.com>
2026-03-31 14:57:32 +0530

    build: configure for github pages deploying to crm.kolabix.com

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
+    build: {
+        outDir: 'docs', // Output to docs for GitHub Pages master/docs deploying
+        emptyOutDir: true
+    }
 })
